1c906e8d9b2f45587ce2140210447446c4a9ed93
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2021-07-17 15:32:54 +0530

    added tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,13 @@
+#include <boost/ut.hpp>
+
+using namespace boost::ut;
+
+unsigned int Factorial(unsigned int number)
+{
+  return number <= 1 ? number : Factorial(number - 1) * number;
+}
+
+suite errors = []{
+  Factorial(1) == 1_i;
+  Factorial(0) == 1_i;
+};
